5a5dc675dc1b080ff07931300a4c67ce8b421caf
Rohan <rohanpawar@gmail.com>
2024-01-20 18:05:08 +0530

    added project activity

--- app/src/main/java/com/example/nrg/MainActivity.kt
+++ app/src/main/java/com/example/nrg/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.nrg
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val Const:ImageButton=findViewById(R.id.Construction);
+
+        Const.setOnClickListener{
+            val intent = Intent(this, Construction::class.java)
+
+            // Start the new activity
+            startActivity(intent)
+        }
+
+
     }
 }
